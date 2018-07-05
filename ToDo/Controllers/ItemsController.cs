c35ffb41b3de849487db1ce229bb369f6c1d80c1
Colin Bates and Nick Rogers <colin@>
2018-07-05 09:59:32 -0700

    Add id and post.

--- ToDo/Controllers/ItemsController.cs
+++ ToDo/Controllers/ItemsController.cs
@@ -8,19 +8,41 @@ namespace ToDo.Controllers
   public class ItemsController : Controller
   {
 
-      [HttpGet("/items")]
-      public ActionResult Index()
-      {
-          Item newItem = new Item(Request.Query["new-item"]);
-          newItem.Save();
-          List<Item> result = new List<Item>();
-          return View(result);
-      }
+    [HttpGet("/items")]
+    public ActionResult Index()
+    {
+        // Item newItem = new Item(Request.Query["new-item"]);
+        // newItem.Save();
+        // List<Item> result = new List<Item>();
+        // return View(result);
+        List<Item> allItems = Item.GetAll();
+        return View(allItems);
+    }
 
-      [HttpGet("/items/new")]
-      public ActionResult CreateForm()
-      {
-          return View();
-      }
+    [HttpGet("/items/new")]
+    public ActionResult CreateForm()
+    {
+        return View();
+    }
+    [HttpPost("/items")]
+    public ActionResult Create()
+    {
+      Item newItem = new Item (Request.Form["new-item"]);
+      // newItem.Save();
+      List<Item> allItems = Item.GetAll();
+      return View("Index", allItems);
+    }
+    [HttpPost("/items/delete")]
+    public ActionResult DeleteAll()
+    {
+        Item.ClearAll();
+        return View();
+    }
+    [HttpGet("/items/{id}")]
+    public ActionResult Details(int id)
+    {
+        Item item = Item.Find(id);
+        return View(item);
+    }
   }
 }
